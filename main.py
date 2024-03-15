1344a45b9690c477a4b3a3d62026c32071a6d31c
Arthur Lino <arthur.ssLino@gmail.com>
2024-03-15 16:05:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from reader import Reader
 
-PATH = "C:/Users/Usuario/AppData/Roaming/JetBrains/PyCharmCE2023.2/scratches/nfe-reader/ad3-louveira"
+PATH = ""
 
 r = Reader(PATH)
 while len(r.files):
